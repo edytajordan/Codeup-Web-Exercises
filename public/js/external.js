faed569571bacc03588578cab050867b045f70b8
Kristen Cates <kristencates@Kristens-MacBook-Air.local>
2016-03-16 13:44:19 -0500

    followed along with ben and entered some if/else statements

--- public/js/external.js
+++ public/js/external.js
@@ -2,3 +2,12 @@
 
 var message = "Hello from external Javascript";
 console.log(message);
+
+if (25 > 24) {
+    //code will only run if true
+    console.log(' first condition was true');
+}else{
+    //code will only run if false
+    console.log('nothing is true');
+}
+
