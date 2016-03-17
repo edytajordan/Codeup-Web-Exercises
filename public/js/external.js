b183687c84a893042a9ed270e71d57e22f7dfd35
Kristen Cates <kristencates@Kristens-MacBook-Air.local>
2016-03-17 09:23:55 -0500

    added some JS from Cameron's lecture on for loops

--- public/js/external.js
+++ public/js/external.js
@@ -1,13 +1,22 @@
 "use strict";
 
-var message = "Hello from external Javascript";
-console.log(message);
+// var message = "Hello from external Javascript";
+// console.log(message);
 
-if (25 > 24) {
-    //code will only run if true
-    console.log(' first condition was true');
-}else{
-    //code will only run if false
-    console.log('nothing is true');
+// if (25 > 24) {
+//     //code will only run if true
+//     console.log(' first condition was true');
+// }else{
+//     //code will only run if false
+//     console.log('nothing is true');
+// }
+
+var i = 0;
+
+while (i < 5) {
+    i++;
 }
 
+for (var i = 0; i < 5; i ++) {
+    console.log(i);
+}
