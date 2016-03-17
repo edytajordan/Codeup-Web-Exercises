c4b01d28368d04552d0123fe3afc590fbe235d3b
Kristen Cates <kristencates@Kristens-MacBook-Air.local>
2016-03-17 17:30:20 -0500

    added code based off of the review questions from 3/17

--- public/js/external.js
+++ public/js/external.js
@@ -25,27 +25,102 @@
 //     console.log(i + ' is an odd number');
 // }
 
-var names = [
-    "Ben",
-    "Isaac",
-    "Melissa",
-    "Will",
-    "Natalie",
-    "Pancho",
-    ]
+// var names = [
+//     "Ben",
+//     "Isaac",
+//     "Melissa",
+//     "Will",
+//     "Natalie",
+//     "Pancho",
+//     ]
 
-    for (var i = 0; i < names.length; i++) {
-        if (names[i].indexOf("i") == -1) {
-            continue;
-        }
+//     for (var i = 0; i < names.length; i++) {
+//         if (names[i].indexOf("i") == -1) {
+//             continue;
+//         }
 
-        console.log(names[i]);
-    }
+//         console.log(names[i]);
+//     }
 
-    for (var i = 0; i < names.length; i++) {
-        if (names[i] == "Will") {
-            break;
-        }
-    }
+//     for (var i = 0; i < names.length; i++) {
+//         if (names[i] == "Will") {
+//             break;
+//         }
+//     }
 
-    console.log("Cool! We found " + names[i]);
+//     console.log("Cool! We found " + names[i]);
+
+// var myAnswer = confirm("This is going to delete EVERYTHING! Are you sure about that?");
+
+// if (myAnswer) {
+//     console.log("DROP ALL THE THINGS!!!!");
+// } else {
+//     console.log("The things are safe");
+// }
+
+// var myName = prompt("What is your name?");
+// console.log(myName);
+
+// var typedValue = prompt("Type into this blank and hit return");
+// console.log("I typed: " + typedValue);
+
+// var emptyValue = prompt("Just hit return");
+// console.log("Empty: " + emptyValue);
+
+// var cancelValue = prompt("Click Cancel");
+// console.log("Canceled: " + cancelValue);
+
+// do {
+//     var requiredvalue = prompt("I demand you answer my questions!");
+// } while (requiredvalue == null || requiredvalue == '' || requiredvalue == "Isaac");
+// console.log(requiredvalue);
+
+// for (var i = 0; i < 10; i++) {   
+//     console.log(i); 
+// }
+
+// var i = 1; 
+// while (true) {   
+//     console.log(i);   
+//     i += 2;   
+//     if (i >= 10) {     
+//         break;   
+//     } 
+// }
+
+// var userName = "Bob";
+// function sayHello (name) {
+//     console.log("Hello " + name + "!");
+// }
+
+// sayHello(userName);
+
+// var userName = "Joe";
+// function sayHello(name) {   
+//     console.log('Hello, ' + name + '!'); 
+// }
+
+// sayHello(userName);
+// var userName = "Zach"
+// function sayHello(name) {   
+//     console.log('Hello, ' + name + '!'); 
+// } 
+// sayHello(userName);
+
+// function multiply(firstNumber, secondNumber){   
+//     var result = firstNumber * secondNumber;
+//     return result;
+//     return firstNumber = secondNumber; 
+// } 
+
+// var answer = multiply(4, 6); 
+// console.log(answer);
+
+function sum(num1, num2) {
+  var result = num1 + num2
+  return result;
+}
+
+function average(num1, num2) {   
+    var result = (sum(num1, num2) / 2);   
+    return result; }
