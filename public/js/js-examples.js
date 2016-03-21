22989b7ec8768b0000c29b1e80d82d2ac5834611
Kristen Cates <kristencates@Kristens-MacBook-Air.local>
2016-03-21 10:43:06 -0500

    Fixed bugs in movie array example

--- public/js/js-examples.js
+++ public/js/js-examples.js
@@ -1,40 +1,40 @@
 (function() {
     "use strict";
     var favoriteMovies = ['The Shawshank Redemption', 'The Color Purple', 'Coming to America', 'Zoolander', "It's Complicated", 'The Breakfast Club', 'Pootie Tang', 'Star Wars: A New Hope', 'Interstellar', 'The Hunt for Red October'];
-    function logMovies() {
+    function logMovies(movieArray) {
         console.log('Here is the list of my favorite movies:');
-        console.log(favoriteMovies);
+        var myFavorite = movieArray.shift();
+        var leastFav = movieArray.pop();
+        console.log("My all time favorite movie is : " + myFavorite);
+        console.log("My least favorite movie is : " + leastFav);
+        console.log("Some other movies are " + movieArray);
     }
 
     // Sort your favorite movies alphabetically
     favoriteMovies.sort();
-    console.log(favoriteMovies);
+    // console.log(favoriteMovies);
 
     // Use .indexOf() to find your number one favorite movie
     var index = favoriteMovies.indexOf('The Color Purple');
-    console.log(index);
+    // console.log(index);
 
     // Use .splice() to take that movie out of the array
     var myFavoriteMovie = favoriteMovies.splice(index, 1);
-    console.log(myFavoriteMovie);
+    // console.log(myFavoriteMovie);
 
     // Put your favorite movie back in at the start of the array
-    var index3 = favoriteMovies.indexOf('The Shawshank Redemption');
-    console.log(index3);
-
-    favoriteMovies.splice(index3+1, 0, myFavoriteMovie[0]);
-    logMovies();
+    favoriteMovies.unshift(myFavoriteMovie[0]);
+   
 
     // Repeat the above steps for your least favorite movie; put it at the end
     var index2 = favoriteMovies.indexOf("It's Complicated");
-    console.log(index2);
+ 
 
     var leastFavorite = favoriteMovies.splice(index2, 1);
-    console.log(leastFavorite);
+    
 
-
-    favoriteMovies.push("It's Complicated");
-    logMovies();
+    favoriteMovies.push(leastFavorite[0]);
+    // logMovies();
 
     // Create a function to console.log() your favorite movies.
         // DO NOT VIOLATE SCOPE! Pass your favoriteMovies array in as a parameter
@@ -44,15 +44,6 @@
         // "Some other movies are [the rest of the movies array with comma space between them]"
         // You should not output any movies twice.
 
-     
-        favoriteMovies.forEach(function (element, index, array) {
-            if (index == 7) {
-                console.log("My all time favorite movie is : " + myFavoriteMovie)
-            } else if ( index == 2) {
-                console.log("My least favorite movie is : " + leastFavorite)
-            } else {
-                var otherMovies = ['The Shawshank Redemption', 'Coming to America', 'Zoolander', 'The Breakfast Club', 'Pootie Tang', 'Star Wars: A New Hope', 'Interstellar', 'The Hunt for Red October']
-            } 
-            console.log("Some other movies are " + otherMovies); 
-            });
+   logMovies(favoriteMovies);
+
 })();
