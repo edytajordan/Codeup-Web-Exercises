83691e71fcdacc6d95b94f513dedc1f7299c0c39
kcates81 <kriscates81@gmail.com>
2016-04-25 10:29:57 -0500

    initial commit

---
+++ public/server-name-generator.php
@@ -0,0 +1,22 @@
+<?php 
+    $nouns = [cat, dog, fish, reptile, bird, amphibian, sock, shoe, pants, shirt];
+
+    $adjectives = [big, small, red, blue, happy, sad, short, tall, fat, skinny];
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Server Generator</title>
+    </head>
+    <body>
+        <?php  
+            $randomNoun = mt_rand($nouns[0], $nouns[count($nouns)];
+            $randomAdjective = mt_rand($adjectives[0], $adjectives[count($adjectives)];
+        ?>
+
+        
+     
+    </body>
+</html>
