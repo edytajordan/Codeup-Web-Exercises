5c26eb2c6f41e8705a0a2f7436e77fe5cf0b6e90
kcates81 <kriscates81@gmail.com>
2016-04-25 10:50:10 -0500

    got the code to work. had some initial errors, but ironed those out. still need to add some CSS.

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,22 +1,22 @@
 <?php 
-    $nouns = [cat, dog, fish, reptile, bird, amphibian, sock, shoe, pants, shirt];
+    $nouns = ['cat', 'dog', 'fish', 'reptile', 'bird', 'amphibian', 'sock', 'shoe', 'pants', 'shirt'];
 
-    $adjectives = [big, small, red, blue, happy, sad, short, tall, fat, skinny];
+    $adjectives = ['big', 'small', 'red', 'blue', 'happy', 'sad', 'short', 'tall', 'fat', 'skinny'];
  ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-
+        <meta charset="UTF-8">
         <title>Server Generator</title>
     </head>
     <body>
         <?php  
-            $randomNoun = mt_rand($nouns[0], $nouns[count($nouns)];
-            $randomAdjective = mt_rand($adjectives[0], $adjectives[count($adjectives)];
+            $randomNoun = mt_rand(0, count($nouns) - 1);
+            $randomAdjective = mt_rand(0, count($adjectives) - 1);
         ?>
+        <p><?php echo $adjectives[$randomAdjective].' '.$nouns[$randomNoun] ?></p>
 
-        
-     
+
     </body>
 </html>
