b119b43d5afea26c24fac8ad70c026df898b6681
kcates81 <kriscates81@gmail.com>
2016-04-25 13:43:26 -0500

    added materialize css

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -7,10 +7,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Favorite Things</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <!-- My CSS Stylesheet -->
+    <link rel="stylesheet" href="/css/my-favorite-things.css">
 </head>
 <body>
-    
-        <table>
+    <div class=" container col s12 m4 l2"></div>
+    <div class="col s12 m4 l8">
+        <table class="striped col s12 m4 l8">
             <tr>
                 <th>My Favorite Things</th>
             </tr>
@@ -22,7 +27,11 @@
                         </td>
                     </tr>
                 <?php }  ?>
-        </table>
-    
+        </table>   
+    </div>
+    <div class="col s12 m4 l2"></div>
+ 
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>   
 </body>
 </html>
