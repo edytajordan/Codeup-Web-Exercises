ee7bb60e02c04e72b65b69efa4b1f86b1f07fac6
kcates81 <kriscates81@gmail.com>
2016-04-25 14:32:16 -0500

    added materialize css stylesheet

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -9,14 +9,21 @@
     <head>
         <meta charset="UTF-8">
         <title>Server Generator</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+        <!-- Customized CSS -->
+        <link rel="stylesheet" href="css/server-name-generator.css">
     </head>
     <body>
-        <?php  
-            $randomNoun = mt_rand(0, count($nouns) - 1);
-            $randomAdjective = mt_rand(0, count($adjectives) - 1);
-        ?>
-        <p><?php echo $adjectives[$randomAdjective].' '.$nouns[$randomNoun] ?></p>
-
-
+        <div class="col s12 m4 l2"></div>
+        <div class="container col s12 m4 l8">
+            <?php  
+                $randomNoun = mt_rand(0, count($nouns) - 1);
+                $randomAdjective = mt_rand(0, count($adjectives) - 1);
+            ?>
+            <h1><?php echo $adjectives[$randomAdjective].' '.$nouns[$randomNoun] ?></h1>
+        </div>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
     </body>
 </html>
