d43853f3906923ed7809195c90e7078cd36edbb3
kcates81 <kriscates81@gmail.com>
2016-04-25 14:24:42 -0500

    added some more materialize classes to my elements to style

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -13,15 +13,16 @@
     <link rel="stylesheet" href="/css/my-favorite-things.css">
 </head>
 <body>
-    <div class=" container col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <table class="striped col s12 m4 l8">
+    <div class="col s12 m4 l2"></div>
+    <div class="container col s12 m4 l8">
+        <h1>Favorite Things Assignment</h1>
+        <table class="highlight z-depth-5 col s12 m4 l8">
             <tr>
-                <th>My Favorite Things</th>
+                <th class="flow-text">My Favorite Things</th>
             </tr>
                 <?php foreach ($favoriteThings as $thing) { ?>
                     <tr>
-                        <td> 
+                        <td class="flow-text"> 
                          <?php echo $thing; ?>
                     
                         </td>
