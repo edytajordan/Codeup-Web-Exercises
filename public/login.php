aaf6fb1369c2a66ef35e21c645a32238b48c3594
kcates81 <kriscates81@gmail.com>
2016-04-28 11:53:11 -0500

    added sessions

--- public/login.php
+++ public/login.php
@@ -1,18 +1,37 @@
 <?php 
 function pageController ()
 {
+    // These variables check to see if a username or password has been inputted into the form 
+    // Otherwise, the input is set to an empty string on page load 
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
+    
+    // This is the initial message displayed on the page 
     $message = 'Please Log In';
 
-    if ($username == 'guest' && $password == 'password') {
+    // This starts my session
+    session_start();
+
+    if (isset($_SESSION['logged_in_user'])) {
+        // This redirects the user to the authorized.php page if they are already logged in
         header('Location:/authorized.php');
         exit();
+    }
+    
+    if ($username == 'guest' && $password == 'password') {
+        // This starts a session and assigns the session key to the username 
+        $_SESSION['logged_in_user'] = $username;
+
+        // This redirects the user to the authorized.php page if they enter the correct login information 
+        header('Location:/authorized.php');
+        exit();
+        
     } elseif ($username !== '') {
         $message = 'Login Failed!';
     }
     
     return ['message' => $message];
+    
 }
 
 extract(pageController());
