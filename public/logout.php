69b877e45553ea79cb4c2bc893e7cfa0577b3084
kcates81 <kriscates81@gmail.com>
2016-04-28 11:53:28 -0500

    intiial commit

---
+++ public/logout.php
@@ -0,0 +1,39 @@
+<?php 
+session_start();
+function pageController () 
+{
+    function clearSession()
+    {
+        // clear $_SESSION array
+        session_unset();
+
+        // delete session data on the server
+        session_destroy();
+
+        // ensure client is sent a new session cookie
+        session_regenerate_id();
+
+        // start a new session - session_destroy() ended our previous session so
+
+        // if we want to store any new data in $_SESSION we must start a new one
+        session_start();
+    }
+
+    clearSession();
+
+    header('Location:/login.php');
+    exit();
+}
+
+extract(pageController());
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logout</title>
+</head>
+<body>
+    <h1>Logout</h1>
+</body>
+</html>
