0ca408e9700c4354e3648c0b19feb0a8e0f90780
kcates81 <kriscates81@gmail.com>
2016-04-28 15:07:18 -0500

    functions file with repeated functions in it to use in other files

--- public/functions.php
+++ public/functions.php
@@ -7,11 +7,11 @@
 
     function inputGet($key)
     {
-
+        return (inputHas($key)) ? $_REQUEST[$key] : null;
     }
 
     function escape($input)
     {
-
+        return strip_tags($input);
     }
 ?>
