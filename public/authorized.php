657c44c5446586897451f5178636aae9f67b567d
kcates81 <kriscates81@gmail.com>
2016-04-28 11:52:48 -0500

    added sessions

--- public/authorized.php
+++ public/authorized.php
@@ -1,9 +1,27 @@
+<?php 
+
+session_start();
+
+function pageController ()
+{
+    if (! isset($_SESSION['logged_in_user'])) {
+        // This redirects the user to the login.php page if they aren't already logged in
+        header('Location:/login.php');
+        exit();
+    } 
+
+    return['username' => $_SESSION['logged_in_user']];
+}
+extract(pageController());
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Authorized</title>
     </head>
     <body>
-        <h1>Authorized</h1>
+        <h1>Authorized <?=$username;?></h1>
+        <a href="/logout.php">Logout</a>
     </body>
 </html>
