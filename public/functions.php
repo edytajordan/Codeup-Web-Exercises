7218631bbe1f29ce9e24ecbe711477fe8bc2462f
kcates81 <kriscates81@gmail.com>
2016-04-28 14:30:52 -0500

    intiial commit

---
+++ public/functions.php
@@ -0,0 +1,17 @@
+<?php  
+    function inputHas($key)
+    {
+        return (isset($_REQUEST[$key]));
+            
+    }
+
+    function inputGet($key)
+    {
+
+    }
+
+    function escape($input)
+    {
+
+    }
+?>
