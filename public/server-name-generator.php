12ea500d95b70f162f959d4d449b45b947b4fcb7
kcates81 <kriscates81@gmail.com>
2016-05-04 17:35:17 -0500

    changes

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -12,24 +12,7 @@
     }
 
     extract(pageController());
+
+    echo "$adjectives"." "."$nouns".PHP_EOL;
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Server Generator</title>
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-        <!-- Customized CSS -->
-        <link rel="stylesheet" href="css/server-name-generator.css">
-    </head>
-    <body>
-        <div class="col s12 m4 l2"></div>
-        <div class="container col s12 m4 l8">
-            <h1><?= "$adjectives"." "."$nouns".PHP_EOL ?></h1>
-        </div>
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-    </body>
-</html>
