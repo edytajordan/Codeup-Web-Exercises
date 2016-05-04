679455ea34928b481767c7cdaf21df3ca9e20778
kcates81 <kriscates81@gmail.com>
2016-05-04 09:06:12 -0500

    fixed error with log and message

--- public/login.php
+++ public/login.php
@@ -5,37 +5,41 @@ require_once '../Input.php';
 // This starts my session
 session_start();
 
+
+
 function pageController()
 {
-    $message = '';
-    
     // This sets my session ID
     $sessionID =session_id();
 
-        // These variables check to see if a username or password has been inputted into the form 
-        // Otherwise, the input is set to an empty string on page load 
-        $username = Input::has('username') ? Input::get('username'): '';
-        $password = Input::has('password') ? Input::get('password'): '';
-        
-        // This is the initial message displayed on the page 
-        $message = 'Please Log In';
+    $message = '';
 
-        if (Auth::check()) {
-            header('Location:/authorized.php');
+    // These variables check to see if a username or password has been inputted into the form 
+    // Otherwise, the input is set to an empty string on page load 
+    $username = Input::has('username') ? Input::get('username'): '';
+    $password = Input::has('password') ? Input::get('password'): '';
+    
+    // This is the initial message displayed on the page 
+    $message = 'Please Log In';
+
+    if (Auth::check()) {
+        header('Location:/authorized.php');
+        exit();
+    }
+   
+   if (!empty($_POST)) {
+       if (Auth::attempt($username, $password)) {
+           header('Location:/authorized.php');
             exit();
-        }
-       
-       if (!empty($_POST)) {
-           if (Auth::attempt($username, $password)) {
-               header('Location:/authorized.php');
-                exit();
-           }
        } else {
-           return $message = 'User {$username} failed to log in'.PHP_EOL;
-       }   
+            $message = "User {$username} failed to log in".PHP_EOL;
+       }
+   } 
+
+   return ['message'=>$message]; 
 }
 
-pageController();
+extract(pageController()) ;
 
 ?>
 
--- public/logout.php
+++ public/logout.php
@@ -17,7 +17,7 @@ function pageController()
     clearSession();      
 }
 
-pageController();
+extract(pageController());
     
 ?>
 
