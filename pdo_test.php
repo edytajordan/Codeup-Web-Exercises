49722c5d1696d62103ba07a6f9fdfbc2b657e2ca
kcates81 <kriscates81@gmail.com>
2016-05-12 10:10:20 -0500

    removed db credentials into separate file

--- pdo_test.php
+++ pdo_test.php
@@ -1,9 +1,6 @@
 <?php  
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME','employees');
-define('DB_USER','codeup');
-define('DB_PASS', 'MySQLPassword123'); 
+require_once 'db_credentials.php'; 
 
 require_once 'db_connect.php';
  
