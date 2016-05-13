8b58b8341855226678abbf0e2f19cff7e14520e7
kcates81 <kriscates81@gmail.com>
2016-05-13 15:03:40 -0500

    changed maxPage from being a hard coded value to a mathematical expression. Also added comments so the code is easier to understand

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,18 +1,23 @@
 <?php  
+    // These are the credentials to access the database 
     define('DB_HOST', '127.0.0.1');
     define('DB_NAME', 'parks_db');
     define('DB_USER', 'parks_user');
     define('DB_PASS', 'ParksDBPassword123');
     
+    // These are the files I need to connect to the MySQL database and to access PHP functions that I use frequently
     require_once '../db_connect.php';
     require_once '../Input.php';
 
+    // These are the variables I use throughout my script 
     $page = Input::get('page');
     $limit = '4'; 
     $offset = $limit * $page;
-
+    $parkTotal = $dbc->query("SELECT count(*) FROM national_parks")->fetchColumn();
+    $maxPage = ($parkTotal/$limit)-1;
     $query = "SELECT * FROM national_parks LIMIT {$limit} OFFSET {$offset}";
 
+    // The following code pulls all of the data from the database and puts it into an array that I can use 
     $stmt = $dbc->query($query);
 
     $parks = [];
@@ -39,6 +44,7 @@
                 <th>Date Established</th>
                 <th>Area in Acres</th>  
             </tr>
+            <!-- This code puts the park data into the correct column based on the key int he associative array i created earlier -->
             <?php foreach ($parks as $park): ?>
                 <tr>
                     <td>
@@ -57,9 +63,14 @@
             <?php endforeach; ?>
         </table>
 
+        <!-- This code controls when/if I see the Previous Page and Next Page Links -->
         <?php if ($page > 0): ?>
             <a href="/national_parks.php?page=<?= $page-1?>">Previous Page</a>
         <?php endif; ?>
-            <a href="/national_parks.php?page=<?=$page+1?>">Next Page</a>
+           
+
+        <?php if ($page < $maxPage): ?>
+             <a href="/national_parks.php?page=<?=$page+1?>">Next Page</a>
+        <?php endif; ?>
     </body>
 </html>
