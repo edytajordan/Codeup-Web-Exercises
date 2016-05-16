918f0066abcca745ecccd54273a3a64cad4d490f
kcates81 <kriscates81@gmail.com>
2016-05-16 11:23:01 -0500

    initial commit

---
+++ public/national_parks_form.php
@@ -0,0 +1,23 @@
+<?php  
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>National Parks Form</title>
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="/css/national-park.css">
+        <!-- Compiled and minified Materialize CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    </head>
+    <body>
+        <h1>Enter Your Favorite National Park</h1>
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <!-- Compiled and minified Materialize JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+    </body>
+</html>
