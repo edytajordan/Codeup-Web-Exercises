f45a89a8bd6025d159f0c5656446646b2de41902
kcates81 <kriscates81@gmail.com>
2016-05-18 11:46:48 -0500

    added magic getter that retries values from the attributes array based on the key name, provided the key exists

--- Model.php
+++ Model.php
@@ -12,5 +12,14 @@ class Model
             // This code allows us to set values to new properties once we've already created an object
             $this->attributes[$key] = $value;
         }
+
+    public function __get($key)
+        {
+            echo "Magic getter called!".PHP_EOL;
+            echo "\$key: $key". PHP_EOL;
+
+            // This checks to see if a value has been set 
+            return isset($this->attributes[$key]) ? $this->attributes[$key] : null;
+        }
 }
 ?>
