1e3dbbcb000affed07c80b48b97a806f667e74ec
kcates81 <kriscates81@gmail.com>
2016-05-18 11:45:08 -0500

    created a magic setter that allows me to create key value pairs in the attributes array

--- Model.php
+++ Model.php
@@ -2,5 +2,15 @@
 class Model
 {
     private $attributes = [];
+
+    public function __set($key, $value)
+        {
+            echo "Magic setter called!".PHP_EOL;
+            echo "\$key: $key".PHP_EOL;
+            echo "\$value: $value".PHP_EOL;
+
+            // This code allows us to set values to new properties once we've already created an object
+            $this->attributes[$key] = $value;
+        }
 }
 ?>
