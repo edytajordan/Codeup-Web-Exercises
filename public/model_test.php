8bbcdeb2f2dc57009cda0e93a212cab89b275423
kcates81 <kriscates81@gmail.com>
2016-05-18 15:36:51 -0500

    changes

--- public/model_test.php
+++ public/model_test.php
@@ -1,11 +1,11 @@
 <?php
     require_once '../Model.php';
 
-    $model = new Model('man', 'Joe');
+    $model = new Model();
 
     //We have created a new property and set its value. We can do this because of our __set() function  
     $model->numberofFingers = 10;
-
+    $model->man = 'Joe';
     echo "-------------------".PHP_EOL;
 
     echo "Key: ".PHP_EOL;
