789941e32de62a61bfe002574eae7f3fce445cbc
kcates81 <kriscates81@gmail.com>
2016-05-20 12:07:28 -0500

    bunch of trys and catches added

--- public/national_parks_form.php
+++ public/national_parks_form.php
@@ -6,23 +6,49 @@
     require_once '../db_connect.php';
     require_once '../Input.php';
 
+    $errors = [];
+
     if (! empty($_POST)) {
 
+        $userInput = $dbc->prepare("INSERT INTO national_parks(name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)");
+
         try {
-            $userInput = $dbc->prepare("INSERT INTO national_parks(name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)");
+            $userInput->bindValue(':name', Input::getString('name'));
 
-            $userInput->bindValue(':name', $_POST['name']);
-            $userInput->bindValue(':location', $_POST['location']);
-            $userInput->bindValue(':date_established', $_POST['date_established']);
-            $userInput->bindValue(':area_in_acres', $_POST['area_in_acres']);
-            $userInput->bindValue(':description', $_POST['description']);
 
-            $userInput->execute();   
-        } catch (Exception $e) {
-            echo "An error occurred".$e->getMessage().PHP_EOL;
+        }catch (Exception $e1){
+            array_push($errors, $e1->getMessage());
         }
-        echo "I'm still working no matter what".PHP_EOL;  
-    }
+
+        try {
+            $userInput->bindValue(':location', Input::getString('location'));   
+        } catch (Exception $e2){
+            array_push($errors, $e2->getMessage());        
+        }
+        
+        try {
+            $userInput->bindValue(':date_established', Input::getDate('date_established'));  
+                
+        } catch (Exception $e3) {
+            array_push($errors, $e3->getMessage());            
+        }    
+        
+        try {
+            $userInput->bindValue(':area_in_acres', Input::getNumber('area_in_acres'));        
+        } catch (Exception $e4) {
+            array_push($errors, $e4->getMessage());         
+        }    
+        
+        try {
+            $userInput->bindValue(':description', Input::getString('description'));       
+        } catch (Exception $e5) {
+            array_push($errors, $e5->getMessage());       
+        }   
+
+        if (empty($errors)) {
+            $userInput->execute(); 
+        }
+    }             
 ?>
 
 <!DOCTYPE html>
@@ -40,30 +66,34 @@
 
         <div class="col s12 m4 l2"></div>
         <div class=" container col s12 m4 l8">
+            <?php if (! empty($errors)){ ?> 
+                <h4><?php var_dump($errors); ?></h4>
+            <?php } ?>
+
             <form class=" container col s12 m4 l8" method="POST" action="national_parks_form.php">
                 <div class="input-field">
                     <label for="name">Name</label>
-                    <input type="text" name="name" placeholder="Park Name">   
+                    <input type="text" name="name" placeholder="Park Name" value="<?= !empty($errors) ? Input::get('name') : '' ?>">   
                 </div>
 
                 <div class="input-field">
                     <label for="location">Location</label>
-                    <input type="text" name="location" placeholder="Park Location">
+                    <input type="text" name="location" placeholder="Park Location"value="<?= !empty($errors) ? Input::get('location') : '' ?>">
                  </div>
                 
                 <div class="input-field">
                     <label for="date_established">Date Established</label>
-                    <input type="text" name="date_established" placeholder="YYYY-MM-DD">    
+                    <input type="text" name="date_established" placeholder="YYYY-MM-DD" value="<?= !empty($errors) ? Input::get('date_established') : '' ?>">    
                 </div>
                 
                 <div class="input-field">
                     <label for="area_in_acres" >Area in Acres</label>
-                    <input type="text" name="area_in_acres" placeholder="Park Area">    
+                    <input type="text" name="area_in_acres" placeholder="Park Area" value="<?= !empty($errors) ? Input::get('area_in_acres') : '' ?>">    
                 </div>
                 
                 <div class="input-field col s12">
                     <label for="description">Description</label>
-                    <textarea class="materialize-textarea" name="description" placeholder="Park Description"></textarea>
+                    <textarea class="materialize-textarea" name="description" placeholder="Park Description" value="<?= !empty($errors) ? Input::get('description') : '' ?>"></textarea>
                 </div>
                 
                 <button class="btn waves-effect waves-light" type="submit" name="action">Submit
