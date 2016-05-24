6989dbb044a5d7c6d80314ecd67079207449aef2
kcates81 <kriscates81@gmail.com>
2016-05-24 11:20:08 -0500

    other users that I can use to populate my user table

---
+++ users_seeder.php
@@ -0,0 +1,39 @@
+<?php  
+$users = [
+            [
+                'name' => 'Kristen Cates',
+                'email' => 'kcates@email.com',
+                'password' => 'dshlkfnkjdaj'
+            ],
+            [
+                'name' => 'Ashley Cates',
+                'email' => 'acates@email.com',
+                'password' => 'uihwefiudhfduihf'
+            ],
+            [
+                'name' => 'Kathy Cates',
+                'email' => 'kpcates@email.com',
+                'password' => 'dqwhbdfhbwfhb'
+            ],
+            [
+                'name' => 'Johhnie Cates',
+                'email' => 'jcates@email.com',
+                'password' => 'hbfjjsdbkd'
+            ],
+            [
+                'name' => 'Tewodros Egziabher',
+                'email' => 'tegziabher@email.com',
+                'password' => 'hfkjaefkejhfbdks'
+            ],
+            [
+                'name' => 'Micah Egziabher',
+                'email' => 'megziabher@email.com',
+                'password' => 'iewubjkdajkndiuh'
+            ],
+            [
+                'name' => 'Isaiah Egziabher',
+                'email' => 'iegziabher@email.com',
+                'password' => 'fjsfjksjk'
+            ],
+        ];
+?>
