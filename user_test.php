aaac2c33809aa974661d10f420578a3a903ce9e1
kcates81 <kriscates81@gmail.com>
2016-05-24 11:18:25 -0500

    tests to create a test user, update users, delete users, and find users

---
+++ user_test.php
@@ -0,0 +1,23 @@
+<?php  
+    require_once'User.php';
+
+    $testUser = new User();
+
+    $testUser->name = 'test';
+    $testUser->email = 'test@email.com';
+    $testUser->password = 'testpassword';
+    
+    $testUser->save();
+
+    // $foundUser = User::find(4);
+    // var_dump($foundUser);
+
+    // $allUsers = User::all();
+    // var_dump($allUsers);
+
+    // $testUser->name = 'Testy McTestface';
+    // $testUser->save();
+    // var_dump($testUser);
+
+    $deleteUser = User::delete(2);
+?>
