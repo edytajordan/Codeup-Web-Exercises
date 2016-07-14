c5aa1da90ece11db01c95b893331186ee62c99f0
kcates81 <kriscates81@gmail.com>
2016-07-14 10:12:40 -0500

    added an array of arrays for the randomizer to loop through

--- public/password_generator.php
+++ public/password_generator.php
@@ -6,5 +6,10 @@ if ($argc == 5 && ctype_digit($argv[1]) && ctype_digit($argv[2]) && ctype_digit(
     $case = $argv[4];
 }
 
+$passwordCharacters = [
+    "alpha" = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    "numeric" = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    "special" = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
 
+];
 ?>
