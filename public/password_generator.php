eb02e2a66ad8df514d33337404acf4b1cb2eb44c
kcates81 <kriscates81@gmail.com>
2016-07-14 09:50:16 -0500

    initial commit. started defining command line arguments and variables

---
+++ public/password_generator.php
@@ -0,0 +1,10 @@
+<?php  
+if ($argc == 5 && ctype_digit($argv[1]) && ctype_digit($argv[2]) && ctype_digit($argv[3] && ctype_alpha($argv[4])) {
+    $passwordLength = $argv[1];
+    $specChars = $argv[2];
+    $digits = $argv[3];
+    $case = $argv[4];
+}
+
+
+?>
