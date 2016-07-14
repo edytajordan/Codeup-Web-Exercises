8178a63559486528276e3fb209fd86dc7e2cb346
kcates81 <kriscates81@gmail.com>
2016-07-14 10:58:52 -0500

    commented out the command line code. trying to see how to get array_rand to work. deleted the multidimensional array and made it an iterative array. for some reason, this gives me 15 random integers in an array, but not 15 random characters from the $passwordCharacters array like I expected. Need to figure out why it is doing that

--- public/password_generator.php
+++ public/password_generator.php
@@ -1,15 +1,20 @@
 <?php  
-if ($argc == 5 && ctype_digit($argv[1]) && ctype_digit($argv[2]) && ctype_digit($argv[3] && ctype_alpha($argv[4])) {
-    $passwordLength = $argv[1];
-    $specChars = $argv[2];
-    $digits = $argv[3];
-    $case = $argv[4];
-}
+// if ($argc == 5 && ctype_digit($argv[1]) && ctype_digit($argv[2]) && ctype_digit($argv[3]) && ctype_alpha($argv[4])) {
+    
+//     $specChars = $argv[2];
+//     $digits = $argv[3];
+//     $case = $argv[4];
+// }
 
-$passwordCharacters = [
-    "alpha" = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-    "numeric" = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    "special" = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
+$passwordCharacters = array(
+        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "?"   
+);
 
-];
+print_r($passwordCharacters);
+
+// $passwordLength = 15;
+
+$password = array_rand($passwordCharacters, 15);
+
+print_r($password);
 ?>
