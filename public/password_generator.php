2e11cb63870d9e4f9f859a5e8c27b5d0b4627988
kcates81 <kriscates81@gmail.com>
2016-07-15 10:26:39 -0500

    found a gist to generate a strong password. this code works if I set $length, $add_dashes, and $available_sets to default values, but lines 4-8 don't work. I get an error message saying that those variables aren't set.

--- public/password_generator.php
+++ public/password_generator.php
@@ -1,20 +1,47 @@
 <?php  
-// if ($argc == 5 && ctype_digit($argv[1]) && ctype_digit($argv[2]) && ctype_digit($argv[3]) && ctype_alpha($argv[4])) {
+function generateStrongPassword($length, $add_dashes, $available_sets)
+{
+    if ($argc == 4 && ctype_digit($argv[1]) && is_bool($argv[2]) &&  ctype_alpha($argv[3])) {
+        $length = $argv[1];
+        $add_dashes = $argv[2];
+        $available_sets = $argv[3];
+    }
     
-//     $specChars = $argv[2];
-//     $digits = $argv[3];
-//     $case = $argv[4];
-// }
+    $sets = array();
+    if(strpos($available_sets, 'l') !== false)
+        $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+    if(strpos($available_sets, 'u') !== false)
+        $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+    if(strpos($available_sets, 'd') !== false)
+        $sets[] = '23456789';
+    if(strpos($available_sets, 's') !== false)
+        $sets[] = '!@#$%&*?';
+    $all = '';
+    $password = '';
+    foreach($sets as $set)
+    {
+        $password .= $set[array_rand(str_split($set))];
+        $all .= $set;
+    }
+    $all = str_split($all);
+    for($i = 0; $i < $length - count($sets); $i++)
+        $password .= $all[array_rand($all)];
+    $password = str_shuffle($password);
+    if(!$add_dashes)
+        return $password;
+    $dash_len = floor(sqrt($length));
+    $dash_str = '';
+    while(strlen($password) > $dash_len)
+    {
+        $dash_str .= substr($password, 0, $dash_len) . '-';
+        $password = substr($password, $dash_len);
+    }
+    $dash_str .= $password;
 
-$passwordCharacters = array(
-        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "?"   
-);
+    return $dash_str;
+}
 
-print_r($passwordCharacters);
+$password = generateStrongPassword($length, $add_dashes, $available_sets);
 
-// $passwordLength = 15;
-
-$password = array_rand($passwordCharacters, 15);
-
-print_r($password);
+echo "Your new password is: " . $password.PHP_EOL;
 ?>
