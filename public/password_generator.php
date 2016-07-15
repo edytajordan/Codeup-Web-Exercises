9a9bd8f984366c3b56681b6171c8983484405fa5
kcates81 <kriscates81@gmail.com>
2016-07-15 11:43:21 -0500

    fixed syntax errors

--- public/password_generator.php
+++ public/password_generator.php
@@ -2,34 +2,49 @@
 function generateStrongPassword($length, $add_dashes, $available_sets)
 {
     $sets = array();
-    if(strpos($available_sets, 'l') !== false)
+    if(strpos($available_sets, 'l') !== false){
         $sets[] = 'abcdefghjkmnpqrstuvwxyz';
-    if(strpos($available_sets, 'u') !== false)
+    }
+    if(strpos($available_sets, 'u') !== false){
         $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
-    if(strpos($available_sets, 'd') !== false)
+    }
+    if(strpos($available_sets, 'd') !== false){
         $sets[] = '23456789';
-    if(strpos($available_sets, 's') !== false)
+    }
+    if(strpos($available_sets, 's') !== false){
         $sets[] = '!@#$%&*?';
+    }
+
     $all = '';
     $password = '';
+
     foreach($sets as $set)
     {
         $password .= $set[array_rand(str_split($set))];
         $all .= $set;
     }
+
     $all = str_split($all);
-    for($i = 0; $i < $length - count($sets); $i++)
+
+    for($i = 0; $i < $length - count($sets); $i++){
         $password .= $all[array_rand($all)];
+    }
+
     $password = str_shuffle($password);
-    if(!$add_dashes)
+
+    if(!$add_dashes){
         return $password;
+    }
+
     $dash_len = floor(sqrt($length));
     $dash_str = '';
+
     while(strlen($password) > $dash_len)
     {
         $dash_str .= substr($password, 0, $dash_len) . '-';
         $password = substr($password, $dash_len);
     }
+
     $dash_str .= $password;
 
     return $dash_str;
