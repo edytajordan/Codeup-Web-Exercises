bd4d5d3cd2baa6f0443ddbf15aacba207d0b199f
kcates81 <kriscates81@gmail.com>
2016-08-09 09:45:17 -0500

    added in some psuedo code to try and figure out how i want to tackle this code

--- public/armstrong.php
+++ public/armstrong.php
@@ -1,3 +1,23 @@
-<?php  
+<?php 
+
+foreach (range(0, 24678050) as $number) {
+    // read number in the range
+
+    // determine number of digits in the number
+
+    // save that number of digits as the exponent
+
+    // raise $number to the power of the exponent
+
+    // get the sum of the digits raised to the power of the exponent
+
+    // compare the sum to $number 
+
+    // if the sum is equal to $number, return $number
+
+    // continue until the program has outputted 25 numbers
+    
+    return $number;
+ } 
 
 ?>
