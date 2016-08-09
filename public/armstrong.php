0b40a935bb42aa023098c90ebb553e9eedc75a98
kcates81 <kriscates81@gmail.com>
2016-08-09 09:58:36 -0500

    started adding code to grab the number of digits for a number

--- public/armstrong.php
+++ public/armstrong.php
@@ -1,13 +1,13 @@
 <?php 
 
 foreach (range(0, 24678050) as $number) {
-    // read number in the range
 
     // determine number of digits in the number
+    $digits = mb_strlen($number);
 
     // save that number of digits as the exponent
 
-    // raise $number to the power of the exponent
+    // raise each digit in $number to the power of the exponent
 
     // get the sum of the digits raised to the power of the exponent
 
@@ -16,7 +16,7 @@ foreach (range(0, 24678050) as $number) {
     // if the sum is equal to $number, return $number
 
     // continue until the program has outputted 25 numbers
-    
+
     return $number;
  } 
 
